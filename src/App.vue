f4ed3e1d14fb5824002d31c928dababcd487370f
Chiara Nuñez <chiaraani@gmail.com>
2023-02-24 15:21:04 +0000

    Transition

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,29 @@
 
 <template>
   <TheHeader />
-  <main><RouterView /></main>
+  <main>
+    <RouterView v-slot="{ Component, route }">
+      <Transition name="fade-slide" mode="out-in">
+        <div :key="route.name">  
+          <component :is="Component" />
+        </div>
+      </Transition>
+    </RouterView>
+  </main>
 </template>
+
+<style>
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  transition: all 0.5s ease;
+}
+.fade-slide-enter-from,
+.fade-slide-leave-to {
+  opacity: 0;
+}
+
+.fade-slide-enter-from { transform:  translateY(-20%); }
+.fade-slide-leave-to { transform:  translateY(20%); }
+
+
+</style>
