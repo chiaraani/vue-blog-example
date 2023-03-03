81c6c4e884a5f2bb0aa6ed8dde2a1567c2469bf8
Chiara Nuñez <chiaraani@gmail.com>
2023-03-03 17:52:29 +0000

    Simulate db createRecord

--- src/data.js
+++ src/data.js
@@ -5,10 +5,18 @@ const fetchJSONFile = async (fileName) => {
   return data.json();
 };
 
+String.prototype.slugify = function() {
+  return this.toLowerCase()
+   .replace(/ /g, '-')
+   .replace(/[^\w-]+/g, '');
+}
+
 const memory = {};
+
 export const findAll = (resource) => {
   return (memory[resource] ||= fetchJSONFile(resource));
 };
+
 export const findRecord = async (resource, slug) => {
   const record = (await findAll(resource)).find(
     (record) => record.slug === slug
@@ -16,3 +24,19 @@ export const findRecord = async (resource, slug) => {
   if (record !== null && record !== undefined) return record;
   else throw new Error("404");
 };
+
+export const createRecord = async (resource, data) => {
+  let id
+  if (memory[resource].constructor === Array) {
+    const lastId = memory[resource][memory[resource].length - 1].id
+    id = lastId + 1
+  } else {
+    memory[resource] = []
+    id = 1
+  }
+
+  const slug = `${id}_${data.title.slugify()}`
+  
+  const newRecord = {id, slug, ...data}
+  memory[resource].push(newRecord)
+}
--- src/tests/data.spec.js
+++ src/tests/data.spec.js
@@ -1,7 +1,7 @@
 import { describe, it, expect, vi } from "vitest";
 
-import { host, findAll, findRecord } from "@/data";
-import { articles, article } from "@/tests/fixtures/articles";
+import { host, findAll, findRecord, createRecord } from "@/data";
+import { articles, article, newArticle } from "@/tests/fixtures/articles";
 
 global.fetch = vi.fn();
 
@@ -19,16 +19,25 @@ describe("findAll", () => {
   });
 });
 
-describe("find", () => {
+describe("findRecord", () => {
   it("returns promise resolved with article found by slug", async () => {
     expect(await findRecord("articles", article.slug)).toEqual(article);
   });
 
   it("throws 404 error if article is not found", async () => {
     expect(
-      await findRecord("articles", "Not a an ID").catch(
-        (error) => error.message === "404"
-      )
+      await findRecord("articles", "Not a an ID")
+      .catch(error => error.message === "404")
     ).toBe(true);
   });
 });
+
+describe('createRecord', () => {
+  it('adds article to list of all articles', async () => {
+    await createRecord('articles', newArticle)
+    expect(
+      await findAll('articles')
+      .find(a => a.title === newArticle.title)
+    ).toEqual(expect.objectContaining(newArticle))
+  })
+})
