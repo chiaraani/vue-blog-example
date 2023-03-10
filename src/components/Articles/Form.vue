6e5c9d5442908c3b79de71015d0c75dc1901d84e
Chiara Nuñez <chiaraani@gmail.com>
2023-03-10 12:20:22 +0000

    Create ArticleForm component

---
+++ src/components/Articles/Form.vue
@@ -0,0 +1,29 @@
+<script setup>
+	import { RouterLink } from 'vue-router'
+
+	const props = defineProps(['article'])
+	const emit = defineEmits(['save'])
+</script>
+
+<template>
+	<article class="card">
+		<form @submit.prevent="emit('save')">
+			<div class="large field">
+				<div>					
+					<label for="title">Title</label>
+					<span class="required-help">*</span>
+				</div>
+				<input type="text" name="title" v-model="article.title"/>
+			</div>
+			<div class="field">
+				<div>	
+					<label for="body">Body</label>
+					<span class="required-help">*</span>
+				</div>
+				<textarea name="body" v-model="article.body"></textarea>
+			</div>
+			<button type="submit">Save</button>
+			<RouterLink :to="{ name: 'home' }" class="red button">Cancel</RouterLink>
+		</form>
+	</article>
+</template>
--- src/views/Articles/New.vue
+++ src/views/Articles/New.vue
@@ -2,39 +2,17 @@
 	import { reactive, } from 'vue'
 
 	import Title from '@/components/layout/Title.vue'
+	import ArticleForm from '@/components/Articles/Form.vue'
 
 	const article = reactive({ 
 		title: '',
 		body: ''
 	})
 
-	const submitHandler = () => console.log('Submitted!', article)
-
-	// Form //
-	import { RouterLink } from 'vue-router'
+	const saveHandler = () => console.log('Submitted!', article)
 </script>
 
 <template>
 	<Title>New article</Title>
-	<!-- Form -->
-	<article class="card">
-		<form @submit.prevent="submitHandler">
-			<div class="large field">
-				<div>					
-					<label for="title">Title</label>
-					<span class="required-help">*</span>
-				</div>
-				<input type="text" name="title" v-model="article.title"/>
-			</div>
-			<div class="field">
-				<div>	
-					<label for="body">Body</label>
-					<span class="required-help">*</span>
-				</div>
-				<textarea name="body" v-model="article.body"></textarea>
-			</div>
-			<button type="submit">Save</button>
-			<RouterLink :to="{ name: 'home' }" class="red button">Cancel</RouterLink>
-		</form>
-	</article>
+	<ArticleForm :article='article' @save="saveHandler" />	
 </template>
