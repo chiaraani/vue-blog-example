ed1ce1d7ce522903bde6bbfc5d7efa67d196dddc
Chiara Nuñez <chiaraani@gmail.com>
2023-03-22 14:53:44 +0000

    Deleting unnecessary helper

--- src/tests/helpers/capitalize.spec.js
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from "vitest";
-
-import capitalize from "@/helpers/capitalize";
-
-describe("capitalize", () => {
-  it("switches first character of string to uppercase", () => {
-    expect(capitalize("john")).toEqual("John");
-  });
-});
